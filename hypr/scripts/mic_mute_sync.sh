b57f628c4ea210fee2855e6dcd280162c8c09f2f
Sandesh Tiwari <sandeshtiwari0211@gmail.com>
2025-11-13 17:10:43 +0545

    Huge Fix: LIGHT/DARK THEME SWITCHERRRRRRRRRRRRRRR WORKINGGGGG FINALLLYYYY

--- hypr/scripts/mic_mute_sync.sh
+++ hypr/scripts/mic_mute_sync.sh
@@ -1,11 +1,11 @@
-#!/usr/bin/env bash
-set -euo pipefail
-LED="/sys/class/leds/platform::micmute/brightness"
+# #!/usr/bin/env bash
+# set -euo pipefail
+# LED="/sys/class/leds/platform::micmute/brightness"
 
-if wpctl get-volume @DEFAULT_AUDIO_SOURCE@ | grep -q '\[MUTED\]'; then
-  VAL=1
-else
-  VAL=0
-fi
+# if wpctl get-volume @DEFAULT_AUDIO_SOURCE@ | grep -q '\[MUTED\]'; then
+#   VAL=1
+# else
+#   VAL=0
+# fi
 
-echo "$VAL" > "$LED" 2>/dev/null || sudo -n tee "$LED" >/dev/null <<<"$VAL"
+# echo "$VAL" > "$LED" 2>/dev/null || sudo -n tee "$LED" >/dev/null <<<"$VAL"
