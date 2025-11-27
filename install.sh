54e45c28532cda156d9e297e2cc9a10b924069ea
Sandesh Tiwari <sandeshtiwari0211@gmail.com>
2025-11-27 17:37:31 +0545

    Update : switched to paru on install.sh and script to set hyprland xdg as default

--- install.sh
+++ install.sh
@@ -1,100 +1,121 @@
 #!/bin/bash
+set -e
+
 
 # Symbolic link, since i am not clutturing my config with unnessery apps and bloat my gitignore augh
-ln -s ~/dotfiles/xdg-desktop-portal ~/.config/xdg-desktop-portal
-ln -s ~/dotfiles/wlogout ~/.config/wlogout
-ln -s ~/dotfiles/waybar ~/.config/waybar
-ln -s ~/dotfiles/swaync ~/.config/swaync
-ln -s ~/dotfiles/rofi ~/.config/rofi
-ln -s ~/dotfiles/pipewire ~/.config/pipewire
-ln -s ~/dotfiles/pavucontrol.ini ~/.config/pavucontrol.ini
-ln -s ~/dotfiles/nvim ~/.config/nvim
-ln -s ~/dotfiles/mimeapps.list ~/.config/mimeapps.list
-ln -s ~/dotfiles/kitty ~/.config/kitty
-ln -s ~/dotfiles/hyprshade ~/.config/hyprshade
-ln -s ~/dotfiles/hypr ~/.config/hypr
-ln -s ~/dotfiles/ghostty ~/.config/ghostty
-ln -s ~/dotfiles/fish ~/.config/fish
-ln -s ~/dotfiles/fastfetch ~/.config/fastfetch
-ln -s ~/dotfiles/cava ~/.config/cava
-ln -s ~/dotfiles/btop ~/.config/btop
-ln -s ~/dotfiles/bleachbit ~/.config/bleachbit
-ln -s ~/dotfiles/gtk-3.0 ~/.config/gtk-3.0
-ln -s ~/dotfiles/gtk-4.0 ~/.config/gtk-4.0
+
+if ! command -v paru >/dev/null 2>&1; then
+  echo "[-] paru not found. Install paru first (on Arch) or use CachyOS."
+  exit 1
+fi
 
 
-# download packages
-# GRAPHICS DRIVERS (IF NOT INSTALLED)
-sudo pacman -S mesa vulkan-radeon
+echo "[*] Creating config symlinks safely..."
+link() {
+    target="$1"
+    link="$2"
 
-# PARU AND YAY
-sudo pacman -S paru yay
+    if [ -e "$link" ] || [ -L "$link" ]; then
+        rm -rf "$link"
+    fi
+
+    ln -s "$target" "$link"
+}
+
+link ~/hypr-dotfile/wlogout ~/.config/wlogout
+link ~/hypr-dotfile/waybar ~/.config/waybar
+link ~/hypr-dotfile/swaync ~/.config/swaync
+link ~/hypr-dotfile/rofi ~/.config/rofi
+link ~/hypr-dotfile/pipewire ~/.config/pipewire
+link ~/hypr-dotfile/pavucontrol.ini ~/.config/pavucontrol.ini
+link ~/hypr-dotfile/nvim ~/.config/nvim
+link ~/hypr-dotfile/mimeapps.list ~/.config/mimeapps.list
+link ~/hypr-dotfile/kitty ~/.config/kitty
+link ~/hypr-dotfile/hyprshade ~/.config/hyprshade
+link ~/hypr-dotfile/hypr ~/.config/hypr
+link ~/hypr-dotfile/ghostty ~/.config/ghostty
+link ~/hypr-dotfile/fish ~/.config/fish
+link ~/hypr-dotfile/fastfetch ~/.config/fastfetch
+link ~/hypr-dotfile/cava ~/.config/cava
+link ~/hypr-dotfile/btop ~/.config/btop
+link ~/hypr-dotfile/bleachbit ~/.config/bleachbit
+link ~/hypr-dotfile/gtk-3.0 ~/.config/gtk-3.0
+link ~/hypr-dotfile/gtk-4.0 ~/.config/gtk-4.0
+
+# BASE PACKAGES
+sudo pacman -Syy --needed base-devel git mesa vulkan-radeon
 
 # HYPRLAND CORE
-sudo pacman -S hyprland hyprlock hyprshade hypridle
-yay -S dwl
+sudo pacman -S --needed hyprland hyprlock hyprshade hypridle
 
 # USED IN WAYBAR
-sudo pacman -S waybar cava fastfetch btop
-yay -S bongocat
+sudo pacman -S --needed waybar cava fastfetch btop
+paru -S bongocat
 
 # USED IN TERMINAL
-sudo pacman -S fish ghostty
+sudo pacman -S --needed fish ghostty
 
 # SOUND
-sudo pacman -S pipewire pipewire-pulse wireplumber pavucontrol
+sudo pacman -S --needed pipewire pipewire-pulse wireplumber pavucontrol
 
 # UTILITIES
-sudo pacman -S swaync xdg-desktop-portal xdg-desktop-portal-gtk xdg-desktop-portal-hyprland networkmanager network-manager-applet playerctl pamixer brightnessctl udiskie wdisplays hyprpolkitagent wltcl wlogout libnotify ntfs-3g sddm os-prober python
+sudo pacman -S --needed swaync xdg-desktop-portal xdg-desktop-portal-gtk xdg-desktop-portal-hyprland networkmanager network-manager-applet playerctl pamixer brightnessctl udiskie wdisplays hyprpolkitagent wltcl wlogout libnotify ntfs-3g sddm os-prober python
 
 # SCREENSHOT / CLIPBOARD
-sudo pacman -S slurp grim wl-clipboard wtype wlroots cliphist
+sudo pacman -S --needed slurp grim wl-clipboard wtype cliphist
 
 # THEME SWITCHER
-sudo pacman -S swww imagemagick jq patch
+sudo pacman -S --needed swww imagemagick jq patch
 
 # ROFI
 paru -S rofi-nerdy
 sudo pacman -S rofi
 
 # APPLICATIONS (dev)
-sudo pacman -S neovim zed obsidian zen-browser git go docker docker-compose lazygit lazydocker
-yay -S spotify helium-browser-bin
+sudo pacman -S --needed neovim zed obsidian git go docker docker-compose lazygit lazydocker
+paru -S spotify helium-browser-bin zen-browser-bin
 
 # APPLICATION GAME
 sudo pacman -S steam
 
 # FONTS
-sudo pacman -S inter-font
-yay -S apple-fonts
+sudo pacman -S --needed inter-font
+paru -S apple-fonts
 
 # APPLICATION GNOME (love gnome apps)
-sudo pacman -S nwg-look gtk3 gtk4 gsettings-desktop-schemas adwaita adwaita-icon-theme dconf glib2 adwaita-cursors loupe papers file-roller gnome-text-editor
+sudo pacman -S --needed nwg-look gtk3 gtk4 gsettings-desktop-schemas adwaita-icon-theme dconf glib2 adwaita-cursors loupe papers file-roller gnome-text-editor
+
+sudo tee /usr/share/xdg-desktop-portal/portals.conf >/dev/null <<EOF
+[preferred]
+default=hyprland
+gtk=gtk
+EOF
 
 # BATTERY CARE
-sudo pacman -S asusctl
-asusctl -c 80 ## limit to 80%
+sudo pacman -S --needed asusctl
+sudo asusctl -c 80 ## limit to 80%
 # disable power-profile-daemon
 sudo systemctl mask power-profiles-daemon
 sudo systemctl disable --now power-profiles-daemon
-yay -S auto-cpufreq
-sudo auto-cpufreq --install
+paru -S auto-cpufreq
+# sudo auto-cpufreq --install
+# do it later after installing everything
 
 
 # Permissions
-chmod +x ~/dotfiles/hypr/scripts/*
-chmod +x ~/dotfiles/rofi/launcher.sh
-chmod +x ~/dotfiles/rofi/wallpapers.sh
-chmod +x ~/dotfiles/waybar/scripts/*
+chmod +x ~/hypr-dotfile/hypr/scripts/*
+chmod +x ~/hypr-dotfile/rofi/*.sh
+chmod +x ~/hypr-dotfile/waybar/scripts/*
 
+
+# ADD USERS TO GROUPS
+sudo usermod -a -G input $USER # for bongocat
+sudo usermod -aG docker $USER # docker
 
 # ENABLE SYSTEM SERVICES
 sudo systemctl enable sddm.service
+sudo systemctl enable NetworkManager.service
 
-# WATCH CONFIGS
-bongocat --watch-config
-sudo usermod -a -G input $USER
-sudo usermod -aG docker $USER
 
 
 
