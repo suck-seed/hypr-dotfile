06c6eba1414af6fa6490e644dfe4ff834f045c62
Sandesh Tiwari <sandeshtiwari0211@gmail.com>
2026-03-02 13:55:22 +0545

    Switched to fleet themes on ghostty, waybar, swaync, fastfetch

--- hypr/shaders/rounded.glsl
+++ hypr/shaders/rounded.glsl
@@ -7,7 +7,7 @@ uniform vec2 screenSize;
 out vec4 fragColor;
 
 // Pixel radius for rounded corners
-const float radiusPx = 14.0;
+const float radiusPx = 8.0;
 // Anti-alias width in pixels (higher = softer edge)
 const float feather = 1.5;
 
